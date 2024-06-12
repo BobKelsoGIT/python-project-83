e29c46053ac5bd113d5b05f598bbf60d6dd33c30
Ivan Scherbakov <lolwut155@gmail.com>
2024-06-12 17:48:11 +0300

    sql fix

--- database.sql
+++ database.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS urls;
+
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(50) UNIQUE NOT NULL ,
+    name varchar(255) UNIQUE NOT NULL ,
     created_at timestamp
 );
