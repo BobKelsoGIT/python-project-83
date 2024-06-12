c724d55093a1de28535ec6e57cbf7821dbbcfadf
Ivan Scherbakov <lolwut155@gmail.com>
2024-06-12 22:17:52 +0300

    app refactored

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,7 +4,9 @@ import psycopg2
 from urllib.parse import urlparse
 import validators
 from dotenv import load_dotenv
-import datetime
+from datetime import datetime
+
+from psycopg2.extras import DictCursor
 
 load_dotenv()
 
@@ -21,15 +23,18 @@ def index():
     )
 
 
-@app.route('/check', methods=['POST'])
-def check_url():
+@app.route('/add', methods=['POST'])
+def add_url():
     current_url = request.form['url']
     if validators.url(current_url):
         url = urlparse(current_url)
         parsed_url = f"{url.scheme}://{url.netloc}"
         conn = psycopg2.connect(DATABASE_URL)
         cur = conn.cursor()
-        cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)", (parsed_url, datetime.datetime.now()))
+        cur.execute(
+            "INSERT INTO urls (name, created_at) VALUES (%s, %s)",
+            (parsed_url, datetime.now().replace(microsecond=0))
+        )
         conn.commit()
         cur.close()
         conn.close()
@@ -39,8 +44,70 @@ def check_url():
 
 @app.route('/urls')
 def urls():
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor(cursor_factory=DictCursor)
+    query_urls = (
+            'SELECT '
+            'urls.id AS id, '
+            'urls.name AS name, '
+            'url_checks.created_at AS last_check, '
+            'urls.created_at as created_at '
+            'FROM urls '
+            'LEFT JOIN url_checks '
+            'ON urls.id = url_checks.url_id '
+            'AND url_checks.id = ('
+            'SELECT max(id) FROM url_checks '
+            'WHERE urls.id = url_checks.url_id) '
+            'ORDER BY urls.id DESC;'
+        )
+    cur.execute(query_urls)
+    urls = cur.fetchall()
+    conn.close()
     return render_template(
         'urls.html',
+        urls=urls,
+    )
+
+
+@app.route('/urls/<int:id>', methods=['GET'])
+def url_info(id):
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor(cursor_factory=DictCursor)
+    query_url = "SELECT * FROM urls WHERE id = %s"
+    cur.execute(query_url, (id,))
+    url = cur.fetchone()
+
+    query_checks = "SELECT * FROM url_checks WHERE url_id = %s"
+    cur.execute(query_checks, (id,)),
+    url_checks = cur.fetchall()
+    print(url_checks)
+    conn.close()
+
+    return render_template(
+        'url_info.html',
+        url=url,
+        url_checks=url_checks,
+    )
+
+
+@app.post('/urls/<int:id>/checks')
+def check_url(id):
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor(cursor_factory=DictCursor)
+    url_id = id
+    query = "INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s);"
+    cur.execute(query, (url_id, datetime.now().replace(microsecond=0)))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return redirect(url_for('url_info', id=id))
+
+
+@app.get('/404')
+def missed_page():
+    return render_template(
+        '404.html',
     )
 
 
