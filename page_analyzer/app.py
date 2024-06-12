f2948db67304ff7b55d6225c13da248ac3c9c182
Ivan Scherbakov <lolwut155@gmail.com>
2024-06-12 17:47:23 +0300

    validators added

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
 import os
 import psycopg2
-
+from urllib.parse import urlparse
+import validators
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,7 +11,6 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
 
 
 @app.route('/')
@@ -18,6 +18,22 @@ def index():
     return render_template(
         'index.html',
     )
+
+
+@app.route('/check', methods=['POST'])
+def check_url():
+    current_url = request.form['url']
+    if validators.url(current_url):
+        url = urlparse(current_url)
+        parsed_url = f"{url.scheme}://{url.netloc}"
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO urls (name) VALUES (%s)", (parsed_url,))
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect(url_for('index'))
 
 
 @app.route('/urls')
@@ -28,4 +44,4 @@ def urls():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
