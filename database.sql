39cf5251024fc3802fe11b4ac5c18cfe6df86c64
Ivan Scherbakov <lolwut155@gmail.com>
2024-06-12 22:17:33 +0300

    database.sql update

--- database.sql
+++ database.sql
@@ -1,7 +1,19 @@
 DROP TABLE IF EXISTS urls;
 
+DROP TABLE url_checks;
+
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) UNIQUE NOT NULL ,
+    created_at timestamp
+);
+
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint,
+    status_code int,
+    h1 varchar(50),
+    title varchar(50),
+    description varchar(255),
     created_at timestamp
 );
