5a1a3b613708dd5551683a04c824b9deb64ec9fe
Ivan Scherbakov <lolwut155@gmail.com>
2024-06-12 16:30:22 +0300

    database added

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, flash
 import os
+import psycopg2
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 
 @app.route('/')
@@ -13,6 +18,7 @@ def index():
     return render_template(
         'index.html',
     )
+
 
 if __name__ == '__main__':
     app.run()
