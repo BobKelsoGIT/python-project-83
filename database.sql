2e169f8aab043a6de959b2ed6d4f35ebeb830bbe
Ivan Scherbakov <lolwut155@gmail.com>
2024-06-17 19:53:25 +0300

    database url_checks fix

--- database.sql
+++ database.sql
@@ -12,8 +12,8 @@ CREATE TABLE url_checks (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id bigint,
     status_code int,
-    h1 varchar(50),
-    title varchar(50),
+    h1 varchar(255),
+    title varchar(255),
     description text,
     created_at date
 );
