5e898d3e99e5fbf0e69d37ae0e8ecf85670e8ecb
Ivan Scherbakov <lolwut155@gmail.com>
2024-06-17 20:53:52 +0300

    db.py refactored

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -9,20 +9,24 @@ def get_db_connection():
     return psycopg2.connect(database_url)
 
 
-def fetch_url_by_name(url_name):
-    query = "SELECT id FROM urls WHERE name = %s"
+def execute_query(query, params=None, fetchone=False, fetchall=False):
     with get_db_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute(query, (url_name,))
-            return cur.fetchone()
+            cur.execute(query, params)
+            if fetchone:
+                return cur.fetchone()
+            elif fetchall:
+                return cur.fetchall()
+
+
+def fetch_url_by_name(url_name):
+    query = "SELECT id FROM urls WHERE name = %s"
+    return execute_query(query, (url_name,), fetchone=True)
 
 
 def insert_url(url, created_at):
     query = "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id"
-    with get_db_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute(query, (url, created_at))
-            return cur.fetchone()[0]
+    return execute_query(query, (url, created_at), fetchone=True)[0]
 
 
 def fetch_urls_with_latest_checks():
@@ -41,26 +45,17 @@ def fetch_urls_with_latest_checks():
         )
         ORDER BY urls.id DESC;
     """
-    with get_db_connection() as conn:
-        with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute(query)
-            return cur.fetchall()
+    return execute_query(query, fetchall=True)
 
 
 def fetch_url_by_id(url_id):
     query = "SELECT * FROM urls WHERE id = %s"
-    with get_db_connection() as conn:
-        with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute(query, (url_id,))
-            return cur.fetchone()
+    return execute_query(query, (url_id,), fetchone=True)
 
 
 def fetch_checks_by_url_id(url_id):
     query = "SELECT * FROM url_checks WHERE url_id = %s"
-    with get_db_connection() as conn:
-        with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute(query, (url_id,))
-            return cur.fetchall()
+    return execute_query(query, (url_id,), fetchall=True)
 
 
 def insert_check(url_id, status_code, h1, title, description):
@@ -68,9 +63,5 @@ def insert_check(url_id, status_code, h1, title, description):
              "(url_id, status_code, h1, title, description, created_at)"
              "VALUES "
              "(%s, %s, %s, %s, %s, %s)")
-    with get_db_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                query,
-                (url_id, status_code, h1, title, description, date.today())
-            )
+    execute_query(query,
+                  (url_id, status_code, h1, title, description, date.today()))
